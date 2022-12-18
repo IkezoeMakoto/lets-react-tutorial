3e2dc998300c1d53e614653fd10faa3a0373d5d1
IkezoeMakoto <150283t@gmail.com>
2022-12-18 14:45:19 +0900

    feat: 履歴を押したときに盤面に反映できるように

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -58,7 +58,7 @@ class Game extends React.Component {
     }
   }
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
@@ -69,7 +69,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
-      stepNumber: 0,
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     })
   }
@@ -81,7 +81,7 @@ class Game extends React.Component {
   }
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
     let status;
     if (winner) {
